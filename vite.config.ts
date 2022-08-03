2c4cb44401b5bf02b10e811228613f99f4df925c
AllenWong <allenwong2407@outlook.com>
2022-08-03 22:54:25 +0800

    use unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from "url";
-import WindiCSS from "vite-plugin-windicss";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import Unocss from "unocss/vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [vue(), Unocss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
