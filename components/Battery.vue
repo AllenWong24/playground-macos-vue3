d55524f19d12f94fff28e94577e0a319c495d061
Neko <46859175+AllenWong24@users.noreply.github.com>
2024-08-25 22:12:14 +0800

    feat: Battery finished

--- components/Battery.vue
+++ components/Battery.vue
@@ -3,7 +3,7 @@ const { level, charging } = useBattery()
 
 const width = computed(() => 0.1 + level.value * 0.96)
 
-function color(): string {
+const color = computed(() => {
   if (charging.value)
     return 'bg-green-400'
   if (level.value < 0.2)
@@ -11,7 +11,7 @@ function color(): string {
   else if (level.value < 0.5)
     return 'bg-yellow-500'
   else return 'bg-white'
-}
+})
 </script>
 
 <template>
@@ -19,13 +19,14 @@ function color(): string {
     <span class="text-xs mr-2">
       {{ (level * 100).toFixed() }}%
     </span>
-    <div class="relative">
-      <Icon name="i-material-symbols-battery-very-low" size="24" />
+    <div class="relative border-2 border-white h-3 w-5 rounded-1">
       <div
-        :class="`battery-level ${color()}`"
+        class="h-full w-full"
+        :class="`${color}`"
         :style="{ width: `${width}rem` }"
       />
-      <Icon v-if="charging" name="i-bi-lightning-charge" size="12" class="absolute top-1/2 -mt-1.5 right-0 mr-1.5" />
+      <Icon v-if="charging" name="i-bi-lightning-charge-fill" size="12" class="absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2" />
+      <div class="w-0.75 h-1 bg-white absolute top-1/2 -translate-y-1/2 -right-1 rounded" />
     </div>
   </div>
 </template>
